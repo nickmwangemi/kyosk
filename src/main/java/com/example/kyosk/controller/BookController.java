704d02e386d7c178bb6df62b418dbf8bf6b5fc24
nickmwangemi <nickmwangemi@gmail.com>
2025-01-10 16:07:35 +0300

    setup book model

--- src/main/java/com/example/kyosk/controller/BookController.java
+++ src/main/java/com/example/kyosk/controller/BookController.java
@@ -1,17 +1,20 @@
 package com.example.kyosk.controller;
 
+import com.example.kyosk.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
 
     @GetMapping("/books")
-    public List getBooks(){
+    public List getBooks() {
         return List.of(
-            new Book("1", "The Hobbit", "J.R.R. Tolkien"),
-            new Book("2", "1984", "George Orwell"),
-            new Book("3", "Pride and Prejudice", "Jane Austen")
+                new Book("1", "The Hobbit", "J.R.R. Tolkien"),
+                new Book("2", "1984", "George Orwell"),
+                new Book("3", "Pride and Prejudice", "Jane Austen")
         );
     }
 
