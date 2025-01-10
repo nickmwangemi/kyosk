d3b52a296c91af359be6c36688d9a79ad919340c
nickmwangemi <nickmwangemi@gmail.com>
2025-01-10 16:05:17 +0300

    setup book controller

---
+++ src/main/java/com/example/kyosk/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.example.kyosk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+
+    @GetMapping("/books")
+    public List getBooks(){
+        return List.of(
+            new Book("1", "The Hobbit", "J.R.R. Tolkien"),
+            new Book("2", "1984", "George Orwell"),
+            new Book("3", "Pride and Prejudice", "Jane Austen")
+        );
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Up and running!";
+    }
+}
